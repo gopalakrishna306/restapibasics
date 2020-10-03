0808116581e2e5697e262804dfc2c89593f9ce3d
Gopalakrishna <51320774+gopalakrishna306@users.noreply.github.com>
2020-10-03 15:25:04 +0530

    Update auth.py
    updated in auth.py

--- restapi/testapp/auth.py
+++ restapi/testapp/auth.py
@@ -22,3 +22,40 @@ class custauthencation(BaseAuthentication):
 #             raise AuthenticationFailed
 #         else:
 #             return (name,None)
+
+
+
+
+
+### updated 
+
+class custauthencation(BaseAuthentication):
+    def authenticate(self, request):
+        # name=request.GET.get('name')
+        tokenn = request.GET.get('token')
+        # if name==None or tokenn==None:
+        if  tokenn == None:
+            raise AuthenticationFailed('please  valid token woth yur end point')
+            # raise AuthenticationFailed('please send valid name and valid token')
+        try:
+            # nam=token.objects.get(name=name,tokenno=tokenn)
+            tokens = token.objects.get(tokenno=tokenn)        # idf token in token model then it will check user in user model and return
+            user = User.objects.get(username=tokens.name)     # and it return corrsponding user is user ntable not in token s table
+            return (user,None)
+        except :
+            raise AuthenticationFailed ('please check your creditials are invalid')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
