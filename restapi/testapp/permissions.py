868dcac18570bcc5b2f5a52e61be58c23a864187
Gopalakrishna <51320774+gopalakrishna306@users.noreply.github.com>
2020-10-03 15:23:54 +0530

    Update permissions.py
    updated in permissions

--- restapi/testapp/permissions.py
+++ restapi/testapp/permissions.py
@@ -10,3 +10,40 @@ class test(BasePermission):
         else:
             return False
 
+        
+        
+#################
+upated with some code in permissions  file
+
+
+
+from rest_framework.permissions import  SAFE_METHODS,BasePermission
+
+class test(BasePermission):
+    def has_permission(self, request, view):
+        print('thisis data',request.user)
+        print(request.user.__dict__)
+        # SAFE_METHODS=['POST',"GET","PUT","PATCH","DELETE"]
+        SAFE_METHODS = ['POST', "GET","DELETE"]
+        # if  request.user.is_staff==True and  request.method in SAFE_METHODS:     ## here iam checking wherterh person is admin or not and
+        if  request.method in SAFE_METHODS:
+            return True                                                          ## and his reqest method also
+        else:
+            return False
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
